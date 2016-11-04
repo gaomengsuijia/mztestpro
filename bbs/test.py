5245c69034ec5aa43914843cccde423ad17c5c78
xiaotu <453105647@qq.com>
2016-11-04 23:44:53 +0800

    modify test

--- bbs/test.py
+++ bbs/test.py
@@ -1,7 +1,22 @@
+#coding:utf-8
+import  os
+def open_report(file_path):
+    files = os.listdir(file_path)
+    #t = os.path.getctime(file_path)
+    files.sort(key=lambda fn: os.path.getmtime(file_path,"\\", + fn))
+    file_new = os.path.join(file_path,files[-1])
+    print(file_new)
+def lis(t):
+    print(t[1])
+    return t[1]
 
-password = "fdsafd"
-def cal(username='',password=password):
-    print(password)
+
+
 
 if __name__ == "__main__":
-    sum = cal()
+    file_path = os.path.dirname(__file__)
+    file_path = file_path + '/report/'
+    #open_report(file_path)
+    L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+    mn = sorted(L,key=lis,reverse=True)
+    print(mn)
--- sendemail.py
+++ sendemail.py
@@ -3,11 +3,17 @@ from email.utils import parseaddr, formataddr
 from email.mime.text import MIMEText
 import smtplib
 from email.header import Header
+import os
 #格式化收件人的显示名字，貌似现在不起作用了
 def _format_addr(s):
     '''格式化一个邮件地址。因为如果包含中文，需要通过Header对象进行编码'''
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
+
+#打开测试报告
+def open_report(file_path):
+    file = file_path + 'result1478008428.605949.html'
+    print(file)
 
 
 # 输入Email地址和口令:
@@ -28,4 +34,9 @@ server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
 server.set_debuglevel(1)#打印发送过程
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
+print("邮件发送成功")
 server.quit()
+
+if __name__ == "__main__":
+    file_path = "./bbs/report/"
+    open_report(file_path)
